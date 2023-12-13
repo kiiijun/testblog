d2569db4cc143674ed1ff1d4a0a68cbc8bdc7741
Kijun Nam <snrm123@naver.com>
2023-12-13 15:35:28 +0900

    upload code

---
+++ db/dbconnect.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Connection to database */
+	/* Azure MySQL Database variable setting */
+	$dbhost = getenv('AZURE_MYSQL_HOST');
+	$dbname = getenv('AZURE_MYSQL_DBNAME');
+	$dbusername = getenv('AZURE_MYSQL_USERNAME');
+	$dbpassword = getenv('AZURE_MYSQL_PASSWORD');
+	$dbport = getenv('AZURE_MYSQL_PORT');
+
+	/* Connect Setting */
+	$conn = mysqli_init();
+	mysqli_real_connect($conn, $dbhost, $dbusername, $dbpassword, $dbname, $dbport)
+
+
+	/* Check connection */
+	if(mysqli_connect_error()) {
+		echo "Connection failed";
+		printf("Error : %s",mysqli_connect_error());
+	}
+
+?>
